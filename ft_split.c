037dbf8658b3f9e0f941bdb1dbd230d46ae65aaa
Diogo Chaves <diogo@enfoquevisual.com.br>
2021-08-24 15:33:18 -0300

    ft_split: working

--- ft_split.c
+++ ft_split.c
@@ -9,29 +9,37 @@
 // The array of new strings resulting from the split. NULL if the allocation
 // fails.
 
-#include "libft.h";
+#include "libft.h"
 
-size_t	ft_strtemp(char *b, char *s, char c)
+size_t	ft_strclean(char *b, const char *s, char c)
 {
 	size_t	counter;
 	size_t	i;
+	size_t	len;
 
 	counter = 0;
 	i = 0;
-	while (*s)
+	while (*s == c)
+		s++;
+	if (*s == '\0')
+		return 0;
+	len = ft_strlen(s);
+	while (s[len-1] == c)
+		len--;
+	while (i < len)
 	{
-		if (*s != c)
+		if (s[i] != c)
 		{
-			*b = *s;
+			*b = s[i];
 			b++;
 		}
 		else if (*(b - 1) != c)
 		{
-			*b = *s;
+			*b = s[i];
 			b++;
 			counter++;
 		}
-		s++;
+		i++;
 	}
 	*b = '\0';
 	return (counter + 1);
@@ -42,25 +50,30 @@ char	**ft_split(char const *s, char c)
 	size_t	counter;
 	size_t	last;
 	size_t	i;
+	size_t	j;
 	char	*buffer;
 	char	**split;
 
 	buffer = malloc(sizeof(s) * (ft_strlen(s) + 1));
-	counter = ft_strtemp(buffer, s, c);
-	split = malloc(sizeof(s)*(counter + 1));
+	counter = ft_strclean(buffer, s, c);
+	if (!counter)
+		return 0;
+	split = malloc(sizeof(char *) * (counter + 1));
 	i = 0;
+	j = 0;
 	last = 0;
-	while(buffer[i])
+	while(buffer[i] != '\0' || counter)
 	{
-		if (buffer[i] == c)
+		if (buffer[i] == c || buffer[i] == '\0')
 		{
-			split = ft_substr(buffer, last, i - last);
-			split++;
+			split[j] = ft_substr(buffer, last, i - last);
+			j++;
 			last = i + 1;
+			counter--;
 		}
 		i++;
 	}
-	*split = '\0';
+	split[j] = 0;
 	free(buffer);
 	return (split);
 }
