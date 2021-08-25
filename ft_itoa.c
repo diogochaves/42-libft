c735b24e00538eeec3faee2be239999d58783a4c
Diogo Chaves <diogo@enfoquevisual.com.br>
2021-08-25 09:35:14 -0300

    ft_itoa: ok

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,38 +5,40 @@
 
 #include "libft.h"
 
+int	count_digits(int n)
+{
+	int	dig;
+
+	dig = 1;
+	while (n != 0)
+	{
+		n = n / 10;
+		if (n != 0)
+			dig++;
+	}
+	return (dig);
+}
+
 char	*ft_itoa(int n)
 {
 	int		neg;
-	int 	sign;
-	int		num;
 	int		dig;
 	char	*num_str;
 
 	neg = 0;
-	sign = 1;
-	num = n;
-	dig = 1;
-	if (num < 0)
-	{
+	dig = count_digits(n);
+	if (n < 0)
 		neg = 1;
-		sign = -1;
-	}
-	while (num != 0)
-	{
-		num = num / 10;
-		if (num != 0)
-			dig++;
-	}
-	num_str = malloc(neg + dig + 1);
+	else
+		n = -n;
+	num_str = malloc(sizeof(char) * (neg + dig + 1));
 	if (!num_str)
 		return (0);
 	num_str[neg + dig] = '\0';
-	num = n;
 	while (dig)
 	{
-		num_str[neg + dig -1] = (num % 10) * sign + '0';
-		num = num / 10;
+		num_str[neg + dig - 1] = -(n % 10) + '0';
+		n /= 10;
 		dig--;
 	}
 	if (neg)
