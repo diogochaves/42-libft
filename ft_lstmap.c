f1dbced0f5c2c2b850c4db432e25c4e76b2c12da
Diogo Chaves <diogo@enfoquevisual.com.br>
2021-09-03 13:13:01 -0300

    ft_lstmap fix

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -18,12 +18,18 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*lst_cpy;
-	t_list	**lst_start;
+	t_list	*lst_start;
 
+	lst_start = 0;
 	while (lst)
 	{
 		lst_cpy = ft_lstnew(f(lst->content));
-		ft_lstadd_back(lst_start, lst_cpy);
+		if (!lst_cpy)
+		{
+			ft_lstclear(&lst_start, del);
+			return (0);
+		}
+		ft_lstadd_back(&lst_start, lst_cpy);
 		lst = lst->next;
 	}
 	return (lst_start);
