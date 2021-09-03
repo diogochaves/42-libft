1f24378dc10d12a9657ef39d6573e720c2154981
Diogo Chaves <diogo@enfoquevisual.com.br>
2021-09-03 14:58:02 -0300

    ft_putnbr_fd fix: remove malloc

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -10,10 +10,24 @@
 
 #include "libft.h"
 
+static void	putint_fd(int n, int fd)
+{
+	if (n >= 10)
+		putint_fd(n / 10, fd);
+	ft_putchar_fd((n % 10) + '0', fd);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*s;
-
-	s = ft_itoa(n);
-	ft_putstr_fd(s, fd);
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	putint_fd(n, fd);
 }
